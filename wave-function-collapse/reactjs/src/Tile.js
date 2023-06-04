da43ae49f910725c29016d866810a8a647dc111b
pradeeptathineni <pradeep.tathineni@gmail.com>
2023-06-04 16:22:55 -0400

    Feat: Wave function collapse semantically works for generating reference tiles, rotating tiles, and maybe assigning tiles

--- wave-function-collapse/reactjs/src/Tile.js
+++ wave-function-collapse/reactjs/src/Tile.js
@@ -6,69 +6,123 @@ import {
     TILE_PORTS,
     PORT_WIDTH,
     PORT_HEIGHT,
+    NUM_POSSIBLE_OPTIONS,
+    PORT_ACTIVE_CHANCE,
 } from "./WaveFunctionCollapse";
 
 const SIDE_COLORS = {
-    0: [0, 255, 0],
-    1: [255, 100, 0],
-    2: [255, 200, 0],
-    3: [0, 150, 255],
+    1: [0, 255, 0],
+    2: [255, 100, 0],
+    3: [255, 255, 0],
+    4: [0, 150, 255],
 };
 
-let ORIENTATION = 0;
+let _rotation = 0;
 
 export default class Tile {
     constructor(
         p,
         index,
-        drawPattern = true,
-        drawOutline = true,
-        drawIndices = true
+        forceOne = false,
+        forceNone = false,
+        forceAll = false,
+        options,
+        ports
     ) {
+        this.options =
+            options === undefined
+                ? [[...Array(NUM_POSSIBLE_OPTIONS).keys()]]
+                : options;
         this.img = p.createGraphics(TILE_WIDTH, TILE_HEIGHT);
         this.img.angleMode(p.DEGREES);
         this.img.rectMode(p.CENTER);
         this.img.translate(TILE_WIDTH / 2, TILE_HEIGHT / 2);
         this.index = index;
-        this.row = Math.floor(this.index / DIMENSION);
-        this.col = this.index % DIMENSION;
+        this.row = this.index % DIMENSION;
+        this.col = Math.floor(this.index / DIMENSION);
         this.x = this.row * TILE_WIDTH;
         this.y = this.col * TILE_HEIGHT;
-        this.ports = [...Array(TILE_PORTS * 4).keys()].map(
-            (index) => new Port(index, this)
-        );
-        this.orientation = 0;
-        this.drawPattern = drawPattern;
-        this.drawOutline = drawOutline;
-        this.drawIndices = drawIndices;
+
+        const generatePorts = () => {
+            let oneActivePortsExists = false;
+            let numActivePorts = 0;
+            let ports = [...Array(TILE_PORTS * 4).keys()].map((portIndex) => {
+                let port = new Port(portIndex, this, true, forceNone, forceAll);
+                // console.log(port);
+                if (port.side > 0 && port.active) {
+                    oneActivePortsExists = true;
+                    numActivePorts++;
+                }
+                if (port.active);
+                return port;
+            });
+
+            if (!forceNone && forceOne && !oneActivePortsExists) {
+                // console.log("We already have an EMPTY tile; regenerating...");
+                return generatePorts();
+            }
+            if (!forceAll && numActivePorts === ports.length - 4) {
+                // console.log("We already have a FULL tile; regenerating...");
+                return generatePorts();
+            }
+
+            return ports;
+        };
+        this.ports = ports === undefined ? generatePorts(ports) : ports;
+
+        // console.log("ports:", this.ports);
+        this.TOP = this.RIGHT = this.BOTTOM = this.LEFT = "";
+        let sideDefinition = {
+            1: "TOP",
+            2: "RIGHT",
+            3: "BOTTOM",
+            4: "LEFT",
+        };
+        for (let i in this.ports) {
+            let port = this.ports[i];
+            if (port.side > 0) {
+                this[sideDefinition[port.side]] += port.active ? "1" : "0";
+            }
+        }
+        const t = this.TOP,
+            r = this.RIGHT,
+            b = this.BOTTOM,
+            l = this.LEFT;
+        this.signatures = [
+            t + r + b + l,
+            r + b + l + t,
+            b + l + t + r,
+            r + b + l + t,
+        ];
+        this.rotation = 0;
         // console.log(
         //     `Tile:\t\t${index}\nw*h:\t\t${TILE_WIDTH}*${TILE_HEIGHT}\n(X, Y):\t\t(${this.x}, ${this.y})\n(row, col):\t\t(${this.row}, ${this.col})`
         // );
     }
 
-    setOrientation(orientation) {
-        if (orientation >= 360) orientation %= 360;
-        this.orientation = orientation;
-        ORIENTATION = orientation;
-        return this.orientation;
+    setRotation(rotation) {
+        if (rotation >= 360) rotation %= 360;
+        this.rotation = rotation;
+        _rotation = rotation;
+        return this.rotation;
     }
 
-    addToOrientation(orientation) {
-        this.orientation += orientation;
-        if (this.orientation >= 360) this.orientation %= 360;
-        ORIENTATION = this.orientation;
+    addToRotation(rotation) {
+        this.rotation += rotation;
+        if (this.rotation >= 360) this.rotation %= 360;
+        _rotation = this.rotation;
 
-        return this.orientation;
+        return this.rotation;
     }
 
     draw(p, drawOutline = true, drawPattern = true, drawIndices = true) {
-        p.push();
         this.img.clear();
+
         if (drawOutline) {
             this.img.push();
             this.img.strokeWeight(1);
             this.img.stroke(255, 0, 0);
-            this.img.rotate(this.orientation);
+            this.img.rotate(this.rotation);
             this.img.rect(0, 0, TILE_WIDTH, TILE_HEIGHT);
             this.img.pop();
         }
@@ -79,7 +133,7 @@ export default class Tile {
                     this.img.push();
                     this.img.strokeWeight(100);
                     this.img.stroke(255, 255, 0);
-                    this.img.rotate(this.orientation);
+                    this.img.rotate(this.rotation);
                     this.drawLineAtPort(p, port);
                     this.img.pop();
                 }
@@ -88,17 +142,30 @@ export default class Tile {
         if (drawIndices) {
             for (let port of this.ports) {
                 this.img.push();
-                this.img.rotate(this.orientation);
-                port.draw(p);
+                this.img.rotate(this.rotation);
+                port.draw();
                 this.img.pop();
             }
+            this.img.push();
+            this.img.fill(255, 200, 0);
+            this.img.strokeWeight(3);
+            this.img.stroke(0);
+            this.img.textAlign(this.img.CENTER);
+            this.img.textSize(14);
+            this.img.textStyle(this.img.BOLD);
+            this.img.text(this.index, 0, 3);
+            this.img.pop();
+            this.img.push();
+            this.img.strokeWeight(1);
+            this.img.fill(0);
+            this.img.text(`(${this.x}, ${this.y})`, 0, 0);
+            this.img.pop();
         }
         p.image(this.img, this.x, this.y);
-        p.pop();
     }
 
     drawLineAtPort(p, port) {
-        this.img.push();
+        p.push();
         this.img.noStroke();
         this.img.fill(0);
         this.img.translate(
@@ -106,13 +173,13 @@ export default class Tile {
             -TILE_HEIGHT / 2 - TILE_HEIGHT * this.col
         );
         const rectWidth = TILE_WIDTH / (TILE_PORTS * 4);
-        const rectHeight = TILE_HEIGHT / 1.42;
+        const rectHeight = TILE_HEIGHT / 2 + rectWidth / 2;
         switch (port.side) {
-            case 0:
+            case 1:
                 this.img.translate(0, rectHeight / 2);
                 this.img.rect(port.x, port.y, rectWidth, rectHeight);
                 break;
-            case 1:
+            case 2:
                 this.img.translate(rectHeight / 2, rectWidth);
                 this.img.rect(
                     port.x - rectHeight,
@@ -121,7 +188,7 @@ export default class Tile {
                     rectWidth
                 );
                 break;
-            case 2:
+            case 3:
                 this.img.translate(rectWidth, 0);
                 this.img.rect(
                     port.x - rectWidth,
@@ -130,7 +197,7 @@ export default class Tile {
                     rectHeight
                 );
                 break;
-            case 3:
+            case 4:
                 this.img.translate(rectHeight / 2, 0);
                 this.img.rect(port.x, port.y, rectHeight, rectWidth);
                 break;
@@ -143,7 +210,7 @@ export default class Tile {
             case -4:
                 break;
         }
-        this.img.pop();
+        p.pop();
     }
 
     randomizeActivePorts() {
@@ -154,25 +221,36 @@ export default class Tile {
 }
 
 class Port {
-    constructor(index, tile, drawIndices = true) {
+    constructor(
+        index,
+        tile,
+        drawIndices = true,
+        forceNone = false,
+        forceAll = false
+    ) {
         this.index = index;
         this.tile = tile;
-        this.active = Math.random() < 0.5;
-        this.side = Math.floor(index / TILE_PORTS);
+        this.side = Math.floor(index / TILE_PORTS) + 1;
+
+        if (this.side > 0) this.active = Math.random() < PORT_ACTIVE_CHANCE;
+        if ((forceNone && !forceAll) || (forceNone && forceAll))
+            this.active = false;
+        if (forceAll && !forceNone) this.active = true;
+
         const tileX = tile.x;
         const tileY = tile.y;
         this.drawIndices = drawIndices;
         const offset = index % TILE_PORTS;
-        if (this.side === 0) {
+        if (this.side === 1) {
             this.x = tileX + offset * PORT_WIDTH;
             this.y = tileY;
-        } else if (this.side === 1) {
+        } else if (this.side === 2) {
             this.x = tileX + TILE_WIDTH;
             this.y = tileY + offset * PORT_HEIGHT;
-        } else if (this.side === 2) {
+        } else if (this.side === 3) {
             this.x = tileX + TILE_WIDTH - offset * PORT_WIDTH;
             this.y = tileY + TILE_HEIGHT;
-        } else if (this.side === 3) {
+        } else if (this.side === 4) {
             this.x = tileX;
             this.y = tileY + TILE_HEIGHT - offset * PORT_HEIGHT;
         } else {
@@ -181,7 +259,7 @@ class Port {
         if (this.index % TILE_PORTS === 0) {
             this.side = -(Math.floor(this.index / TILE_PORTS) + 1);
         }
-        this.orientation = ORIENTATION;
+        this.rotation = _rotation;
         // console.log(`Tile (X, Y): (${tileX}, ${tileY})\nPort (Index, X, Y): (${this.index}, ${this.x}, ${this.y})`);
     }
 
@@ -192,9 +270,9 @@ class Port {
             -TILE_HEIGHT / 2 - TILE_HEIGHT * this.tile.col
         );
         this.tile.img.stroke(0);
-        this.tile.img.strokeWeight(5);
+        this.tile.img.strokeWeight(10);
         let sideColor = [100, 100, 100, 100];
-        if (this.side >= 0) {
+        if (this.side > 0) {
             sideColor = SIDE_COLORS[this.side];
         }
         this.tile.img.stroke(...sideColor);
@@ -203,43 +281,46 @@ class Port {
             this.tile.img.stroke(...sideColor);
             const textOffset = 15;
             let xOff = 0,
-                yOff = 0;
+                yOff = 3;
             switch (this.side) {
-                case 0:
-                    xOff = this.x;
-                    yOff = this.y + textOffset;
-                    break;
                 case 1:
-                    xOff = this.x - textOffset;
-                    yOff = this.y;
+                    xOff += this.x;
+                    yOff += this.y + textOffset;
                     break;
                 case 2:
-                    xOff = this.x;
-                    yOff = this.y - textOffset;
+                    xOff += this.x - textOffset;
+                    yOff += this.y;
                     break;
                 case 3:
-                    xOff = this.x + textOffset * 0.5;
-                    yOff = this.y;
+                    xOff += this.x;
+                    yOff += this.y - textOffset;
+                    break;
+                case 4:
+                    xOff += this.x + textOffset;
+                    yOff += this.y;
                     break;
                 case -1:
-                    xOff = this.x + textOffset;
-                    yOff = this.y + textOffset;
+                    xOff += this.x + textOffset;
+                    yOff += this.y + textOffset;
                     break;
                 case -2:
-                    xOff = this.x - textOffset;
-                    yOff = this.y + textOffset;
+                    xOff += this.x - textOffset;
+                    yOff += this.y + textOffset;
                     break;
                 case -3:
-                    xOff = this.x - textOffset;
-                    yOff = this.y - textOffset;
+                    xOff += this.x - textOffset;
+                    yOff += this.y - textOffset;
                     break;
                 case -4:
-                    xOff = this.x + textOffset;
-                    yOff = this.y - textOffset;
+                    xOff += this.x + textOffset;
+                    yOff += this.y - textOffset;
                     break;
             }
+            this.tile.img.strokeWeight(5);
+            this.tile.img.textAlign(this.tile.img.CENTER);
             this.tile.img.textFont("Georgia");
             this.tile.img.textSize(10);
+            this.tile.img.fill(0);
             this.tile.img.text(this.index, xOff, yOff);
         }
         this.tile.img.pop();
--- wave-function-collapse/reactjs/src/WaveFunctionCollapse.js
+++ wave-function-collapse/reactjs/src/WaveFunctionCollapse.js
@@ -2,47 +2,213 @@ import React, { useRef, useEffect } from "react";
 import p5 from "p5";
 import Tile from "./Tile";
 
-export const DIMENSION = 3; // set this to 1 to operate on one tile as base case
 export const WIDTH = window.innerHeight;
 export const HEIGHT = window.innerHeight;
-export const TILE_WIDTH = Math.ceil(WIDTH / DIMENSION);
-export const TILE_HEIGHT = Math.ceil(HEIGHT / DIMENSION);
-export const TILE_PORTS = 3;
+
+export const DIMENSION = 3;
+
+export const TILE_WIDTH = WIDTH / DIMENSION;
+export const TILE_HEIGHT = HEIGHT / DIMENSION;
+
+const NUM_TILE_PORTS = 3;
+export const TILE_PORTS = NUM_TILE_PORTS + 1;
+
 export const PORT_WIDTH = TILE_WIDTH / TILE_PORTS;
 export const PORT_HEIGHT = TILE_HEIGHT / TILE_PORTS;
+export const PORT_ACTIVE_CHANCE = 0.6;
+
+const REDUCE_OPTIONS_FACTOR = 4;
+export const NUM_POSSIBLE_OPTIONS = Math.ceil(
+    DIMENSION ** 2 / REDUCE_OPTIONS_FACTOR
+);
+
+const INCLUDE_EMPTY_TILE = false;
+const INCLUDE_FULL_TILE = false;
+
+let TOGGLE = 0;
 
 export default function WaveFunctionCollapse() {
     const canvasRef = useRef(null);
 
     useEffect(() => {
         const sketch = new p5((p) => {
-            let tiles = [];
+            let referenceTiles = [];
+            let collapsingTiles = [];
 
             p.setup = () => {
                 p.createCanvas(WIDTH, HEIGHT);
                 p.angleMode(p.DEGREES);
                 p.rectMode(p.CENTER);
-                // p.frameRate(1);
-                for (let i = 0; i < DIMENSION ** 2; i++) {
-                    tiles.push(new Tile(p, i));
+                // p.noLoop();
+                p.frameRate(1);
+                p.push();
+                if (DIMENSION === 1) {
+                    let tile = new Tile(p, 0, true);
+                    referenceTiles.push(tile);
+                } else {
+                    for (let i = 0; i < NUM_POSSIBLE_OPTIONS; i++) {
+                        let shouldBeEmpty = false,
+                            shouldBeFull = false;
+                        if (INCLUDE_EMPTY_TILE && INCLUDE_FULL_TILE) {
+                            if (i === 0) shouldBeEmpty = true;
+                            if (i === 1) shouldBeFull = true;
+                        } else if (!INCLUDE_EMPTY_TILE && INCLUDE_FULL_TILE) {
+                            if (i === 0) shouldBeFull = true;
+                        }
+                        let tile = new Tile(
+                            p,
+                            i,
+                            true,
+                            shouldBeEmpty,
+                            shouldBeFull
+                        );
+                        referenceTiles.push(tile);
+                        // console.log(tile);
+                    }
                 }
+                p.pop();
+                p.push();
+                for (let i = 0; i < DIMENSION ** 2; i++) {
+                    let tile = new Tile(p, i, false, true, false);
+                    collapsingTiles.push(tile);
+                }
+                p.pop();
+                console.log("referenceTiles", referenceTiles);
+                console.log("collapsingTiles", collapsingTiles);
+                collapse(true);
             };
 
             p.draw = () => {
-                for (let i = 0; i < tiles.length; i++) {
-                    p.push();
-                    let tile = tiles[i];
+                p.clear();
+                let drawTheseTiles =
+                    TOGGLE === 0 ? collapsingTiles : referenceTiles;
+                for (let i = 0; i < drawTheseTiles.length; i++) {
+                    // collapse();
+                    let tile = drawTheseTiles[i];
                     tile.draw(p);
                 }
             };
 
             p.mouseClicked = () => {
-                for (let i = 0; i < tiles.length; i++) {
-                    let tile = tiles[i];
-                    let o = tile.addToOrientation(90);
-                    // console.log(`tile ${i} orientation = ${o}`);
-                }
+                TOGGLE++;
+                if (TOGGLE >= 2) TOGGLE = 0;
             };
+
+            function collapse(randomly = false) {
+                let referenceChoiceOffset =
+                    INCLUDE_EMPTY_TILE * 1 + INCLUDE_FULL_TILE * 1;
+                let leastOptions = Number.NEGATIVE_INFINITY;
+                let referenceIndex, collapsingIndex;
+
+                if (randomly) {
+                    if (DIMENSION === 1) {
+                        referenceIndex = 0;
+                    } else if (DIMENSION === 2) {
+                        referenceIndex = referenceChoiceOffset;
+                    }
+                    referenceIndex =
+                        DIMENSION > 1
+                            ? Math.floor(
+                                  Math.random() *
+                                      (referenceTiles.length -
+                                          referenceChoiceOffset)
+                              ) + referenceChoiceOffset
+                            : 0;
+                } else {
+                    referenceIndex = Math.floor(
+                        Math.random() * referenceTiles.length
+                    );
+                }
+
+                let equalLeastOptions = [];
+                if (randomly) {
+                    equalLeastOptions.push(
+                        Math.floor(Math.random() * collapsingTiles.length)
+                    );
+                } else {
+                    for (let i = 0; i < collapsingTiles.length; i++) {
+                        console.log("Before:", equalLeastOptions);
+                        let tile = collapsingTiles[i];
+                        let numOptions = tile.options.length;
+                        if (numOptions <= leastOptions) {
+                            if (numOptions < leastOptions) {
+                                equalLeastOptions = [];
+                                leastOptions = numOptions;
+                            }
+                            equalLeastOptions.push(i);
+                        }
+                        console.log("After:", equalLeastOptions);
+                    }
+                }
+                console.log("equalLeastOptions", equalLeastOptions);
+                if (equalLeastOptions.length > 1) {
+                    collapsingIndex =
+                        equalLeastOptions[
+                            Math.floor(
+                                Math.random() * (equalLeastOptions.length - 1)
+                            )
+                        ];
+                } else {
+                    collapsingIndex = equalLeastOptions[0];
+                }
+                const referenceTile = referenceTiles[referenceIndex];
+                console.log("referenceTile", referenceTile);
+                if (randomly) {
+                    console.log(
+                        "Before collapsingTile:",
+                        collapsingTiles[collapsingIndex]
+                    );
+                    collapsingTiles[collapsingIndex] = new Tile(
+                        p,
+                        collapsingIndex
+                    );
+                    collapsingTiles[collapsingIndex].TOP = referenceTile.TOP;
+                    collapsingTiles[collapsingIndex].RIGHT =
+                        referenceTile.RIGHT;
+                    collapsingTiles[collapsingIndex].BOTTOM =
+                        referenceTile.BOTTOM;
+                    collapsingTiles[collapsingIndex].LEFT = referenceTile.LEFT;
+                    collapsingTiles[collapsingIndex].ports.map((port) => {
+                        const referencePorts = referenceTile.ports;
+                        let referencePort;
+                        for (let idx in referencePorts) {
+                            const refPort = referencePorts[idx];
+                            if (refPort.index === port.index) {
+                                referencePort = refPort;
+                            }
+                        }
+                        // console.log("ref port:", referencePort);
+                        // console.log("coll port:", port);
+                        port.active = referencePort.active;
+                        // port.row = port.index % DIMENSION;
+                        // port.col = Math.floor(port.index / DIMENSION);
+                        // port.x = port.row * TILE_WIDTH;
+                        // port.y = port.col * TILE_HEIGHT;
+                        // port.tile = collapsingTiles[collapsingIndex];
+                        return port;
+                    });
+                    collapsingTiles[collapsingIndex].rotation += 90;
+
+                    console.log(
+                        "After collapsingTile:",
+                        collapsingTiles[collapsingIndex]
+                    );
+                }
+            }
+
+            function removeDuplicateTiles(tiles) {
+                return tiles.filter(
+                    (tile, index, self) =>
+                        index ===
+                        self.findIndex(
+                            (t) =>
+                                t.TOP === tile.TOP &&
+                                t.RIGHT === tile.RIGHT &&
+                                t.BOTTOM === tile.BOTTOM &&
+                                t.LEFT === tile.LEFT
+                        )
+                );
+            }
         }, canvasRef.current);
 
         return sketch.remove;
